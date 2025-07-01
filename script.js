a3ba27f80432ae52b02a17200b7858bfea339c66
Sanskar pandey. <sanskar.pandey7766@gmail.com>
2025-07-01 13:24:57 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const div = document.createElement("div");
+  div.innerText = "JS.ORG verified: JavaScript is working!";
+  div.style.color = "blue";
+  div.style.fontWeight = "bold";
+  document.body.appendChild(div);
+});
